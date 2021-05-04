a5d4f3b1e573590e584893b017cc598cae6014a0
Geine <cho881020@gmail.com>
2021-05-04 16:48:54 +0900

    ContextUtil 추가 -> 자동로그인 저장 기능   - 저장된 자동로그인 여부 확인 작업 필요

--- app/src/main/java/com/neppplus/sharedpreftest_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/sharedpreftest_genie/MainActivity.kt
@@ -2,10 +2,14 @@ package com.neppplus.sharedpreftest_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.sharedpreftest_genie.utils.ContextUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
---
+++ app/src/main/java/com/neppplus/sharedpreftest_genie/utils/ContextUtil.kt
@@ -0,0 +1,22 @@
+package com.neppplus.sharedpreftest_genie.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        private val prefName = "TestPref"
+
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+        fun setAutoLogin(context: Context, autoLogin: Boolean) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+
+        }
+
+    }
+
+}
